14a8523f60ffb794b377e92618253cbb549c2ff9
vastpn <641597345@qq.com>
2018-09-18 10:20:25 +0800

    分布式配置中心[测试通过，客户端可通过Config 获取GIT仓库配置文件]

---
+++ vastpn-config/src/main/java/com/vastpn/ms/configserver/ConfigServerApplication.java
@@ -0,0 +1,30 @@
+package com.vastpn.ms.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * <pre>
+ * <b>.</b>
+ * <b>Description:</b>
+ *
+ * <b>Author: 641597345@qq.com </b>
+ * <b>Date: 2018/9/18 0018 10:04   </b>
+ * <b>Copyright:</b> Copyright 2008-2026 http://www.jinvovo.com Technology Co., Ltd. All rights reserved.
+ * <b>Changelog:</b>
+ *   ----------------------------------------------------------------------------
+ *   Ver    Date                     Author                        Detail
+ *   ----------------------------------------------------------------------------
+ *   1.0   2018/9/18 0018 10:04          641597345@qq.com            new file.
+ * <pre>
+ */
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
