45cab87a0ef84bbc84defd8a05562660e7efd151
vastpn <641597345@qq.com>
2018-10-10 09:14:14 +0800

    eureka、config、admin增加security。

---
+++ vastpn-order/src/main/java/com/vastpn/ms/order/web/config/ControllerSecurityConfiguration.java
@@ -0,0 +1,70 @@
+package com.vastpn.ms.order.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * <pre>
+ * <b>.</b>
+ * <b>Description:</b>
+ *
+ * <b>Author: 641597345@qq.com </b>
+ * <b>Date: 2018/10/9 0009 14:44   </b>
+ * <b>Copyright:</b> Copyright 2008-2026 http://www.jinvovo.com Technology Co., Ltd. All rights reserved.
+ * <b>Changelog:</b>
+ *   ----------------------------------------------------------------------------
+ *   Ver    Date                     Author                        Detail
+ *   ----------------------------------------------------------------------------
+ *   1.0   2018/10/9 0009 14:44          641597345@qq.com            new file.
+ * <pre>
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, proxyTargetClass = true)
+public class ControllerSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //忽略css.jq.img等文件
+        web.ignoring().antMatchers("/**.html","/**.css", "/img/**", "/**.js","/third-party/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable() //HTTP with Disable CSRF
+                .authorizeRequests() //Authorize Request Configuration
+                .antMatchers( "/login",
+                        "/api/**",
+                        "/order/**",
+                        "/**/heapdump",
+                        "/**/loggers",
+                        "/**/liquibase",
+                        "/**/logfile",
+                        "/**/flyway",
+                        "/**/auditevents",
+                        "/**/jolokia").permitAll() //放开"/api/**"：为了给被监控端免登录注册并解决Log与Logger冲突
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").hasRole("USER")
+                .antMatchers("/**").hasRole("SUPERUSER")
+                .antMatchers("/**/**").authenticated()
+                .and() //Login Form configuration for all others
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/login").permitAll()
+                .defaultSuccessUrl("/")
+                .and() //Logout Form configuration
+                .logout()
+                .deleteCookies("remove")
+                .logoutSuccessUrl("/login.html").permitAll()
+                .and()
+                .httpBasic();
+
+    }
+}
